a174de43bad27925550c4e4d496bbd2044b6982b
Wolkenfressor <huhityt@gmail.com>
2025-05-26 23:10:38 +0300

    коммит три

--- cypress/e2e/constructor-e2e.cy.ts
+++ cypress/e2e/constructor-e2e.cy.ts
@@ -1,128 +1,99 @@
-describe('Тестирование конструктора бургеров', () => {
-  const baseUrl = Cypress.config('baseUrl')!;
-  const modal = '[data-cy="modal"]';
-  const overlay = '[data-cy="modal-overlay"]';
-  const bun = '[data-cy="bun"]';
-  const ingredients = '[data-cy="ingredients"]';
-  const bunConstructor = '[data-cy="bun-constructor"]';
-  const itemConstructor = '[data-cy="ingredients-constructor"]';
+// burgerConstructor.spec.ts
 
-  beforeEach(() => {
-    cy.fixture('ingredients.json').then((data) => {
-      cy.intercept('GET', 'api/ingredients', {
-        statusCode: 200,
-        body: data
-      });
+describe('Поведение конструктора бургеров', () => {
+    const selectors = {
+        modal: '[data-cy="modal"]',
+        overlay: '[data-cy="modal-overlay"]',
+        bunBlock: '[data-cy="bun"]',
+        fillBlock: '[data-cy="ingredients"]',
+        topBun: '[data-cy="bun-constructor"]',
+        fillingArea: '[data-cy="ingredients-constructor"]',
+    };
+
+    const labels = {
+        bunName: 'Краторная булка N-200i',
+        meat: 'Филе Люминесцентного тетраодонтимформа',
+        sauce: 'Соус традиционный галактический',
+    };
+
+    const setupRoutesAndVisit = () => {
+        cy.fixture('ingredients.json').then((ingredients) => {
+            cy.intercept('GET', 'api/ingredients', { statusCode: 200, body: ingredients });
+        });
+
+        cy.fixture('user.json').then((user) => {
+            cy.intercept('GET', 'api/auth/user', { statusCode: 200, body: user });
+        });
+
+        cy.fixture('order.json').then((order) => {
+            cy.intercept('POST', 'api/orders', { statusCode: 200, body: order }).as('orderRequest');
+        });
+
+        cy.setCookie('accessToken', 'exampleAccessToken');
+        cy.setCookie('refreshToken', 'exampleRefreshToken');
+
+        cy.visit(Cypress.config('baseUrl')!);
+    };
+
+    const addIngredient = (name: string) => {
+        cy.contains('li', name).contains('Добавить').click();
+    };
+
+    beforeEach(setupRoutesAndVisit);
+
+    context('Модальное окно ингредиента', () => {
+        it('Открытие и закрытие модального окна по кнопке и по фону', () => {
+            cy.contains('li', labels.bunName).click();
+            cy.get(selectors.modal).should('contain', labels.bunName);
+            cy.get(selectors.modal).find('button').click();
+            cy.get(selectors.modal).should('not.exist');
+
+            cy.contains('li', labels.meat).click();
+            cy.get(selectors.modal).should('contain', labels.meat);
+            cy.get(selectors.overlay).click({ force: true });
+            cy.get(selectors.modal).should('not.exist');
+        });
     });
 
-    cy.fixture('user.json').then((data) => {
-      cy.intercept('GET', 'api/auth/user', {
-        statusCode: 200,
-        body: data
-      }).as('fetchUser');
+    it('Булка добавляется в начало и конец конструктора', () => {
+        cy.get(selectors.topBun).should('not.contain', labels.bunName);
+        addIngredient(labels.bunName);
+        cy.get(selectors.bunBlock).should('contain', labels.bunName);
     });
 
-    cy.fixture('order.json').then((data) => {
-      cy.intercept('POST', 'api/orders', {
-        statusCode: 200,
-        body: data
-      }).as('createOrder');
+    it('Добавление ингредиентов и соуса внутрь бургера', () => {
+        addIngredient(labels.bunName);
+        cy.get(selectors.bunBlock).should('contain', labels.bunName);
+
+        addIngredient(labels.meat);
+        cy.get(selectors.fillBlock).should('contain', labels.meat);
+
+        addIngredient(labels.sauce);
+        cy.get(selectors.fillBlock).should('contain', labels.sauce);
     });
 
-    cy.setCookie('accessToken', 'exampleAccessToken');
-    cy.setCookie('refreshToken', 'exampleRefreshToken');
+    describe('Завершение заказа', () => {
+        const completeOrderFlow = () => {
+            addIngredient(labels.bunName);
+            addIngredient(labels.meat);
+            addIngredient(labels.sauce);
+        };
 
-    cy.visit(baseUrl);
-  });
+        it('Корректное оформление и очистка конструктора', () => {
+            completeOrderFlow();
 
-  it('открывает и закрывает модальное окно по кнопке и оверлею', () => {
-    cy.contains('li', 'Краторная булка N-200i').click();
-    cy.get(modal).should('contain', 'Краторная булка N-200i');
+            cy.contains('button', 'Оформить заказ').click();
+            cy.wait('@orderRequest');
+            cy.get(selectors.modal).should('contain', '12345');
+            cy.get(selectors.modal).find('button').click();
+            cy.get(selectors.modal).should('not.exist');
 
-    cy.get(modal).find('button').click();
-    cy.get(modal).should('not.exist');
+            cy.get(selectors.topBun).should('contain', 'Выберите булки');
+            cy.get(selectors.fillingArea).should('contain', 'Выберите начинку');
+        });
 
-    cy.contains('li', 'Филе Люминесцентного тетраодонтимформа').click();
-    cy.get(modal).should('contain', 'Филе Люминесцентного тетраодонтимформа');
-
-    cy.get(overlay).click({ force: true });
-    cy.get(modal).should('not.exist');
-  });
-
-  it('добавляет булку в конструктор', () => {
-    cy.get(bunConstructor).should('not.contain', 'Краторная булка N-200i');
-    cy.contains('li', 'Краторная булка N-200i').contains('Добавить').click();
-    cy.get(bun).should('contain', 'Краторная булка N-200i');
-  });
-
-  it('добавляет начинку и соус в конструктор', () => {
-    cy.get(bunConstructor).should('not.contain', 'Краторная булка N-200i');
-    cy.contains('li', 'Краторная булка N-200i').contains('Добавить').click();
-    cy.get(bun).should('contain', 'Краторная булка N-200i');
-
-    cy.get(ingredients).should(
-      'not.contain',
-      'Филе Люминесцентного тетраодонтимформа'
-    );
-    cy.contains('li', 'Филе Люминесцентного тетраодонтимформа')
-      .contains('Добавить')
-      .click();
-    cy.get(ingredients).should(
-      'contain',
-      'Филе Люминесцентного тетраодонтимформа'
-    );
-
-    cy.get(ingredients).should(
-      'not.contain',
-      'Соус традиционный галактический'
-    );
-    cy.contains('li', 'Соус традиционный галактический')
-      .contains('Добавить')
-      .click();
-    cy.get(ingredients).should('contain', 'Соус традиционный галактический');
-  });
-
-  describe('Оформление заказа', () => {
-    it('создает заказ и очищает конструктор', () => {
-      cy.get(bunConstructor).should('not.contain', 'Краторная булка N-200i');
-      cy.contains('li', 'Краторная булка N-200i').contains('Добавить').click();
-      cy.get(bun).should('contain', 'Краторная булка N-200i');
-
-      cy.get(ingredients).should(
-        'not.contain',
-        'Филе Люминесцентного тетраодонтимформа'
-      );
-      cy.contains('li', 'Филе Люминесцентного тетраодонтимформа')
-        .contains('Добавить')
-        .click();
-      cy.get(ingredients).should(
-        'contain',
-        'Филе Люминесцентного тетраодонтимформа'
-      );
-
-      cy.get(ingredients).should(
-        'not.contain',
-        'Соус традиционный галактический'
-      );
-      cy.contains('li', 'Соус традиционный галактический')
-        .contains('Добавить')
-        .click();
-      cy.get(ingredients).should('contain', 'Соус традиционный галактический');
-
-      cy.contains('button', 'Оформить заказ').click();
-      cy.wait('@createOrder');
-
-      cy.get(modal).should('contain', '12345');
-      cy.get(modal).find('button').click();
-      cy.get(modal).should('not.exist');
-
-      cy.get(bunConstructor).should('contain', 'Выберите булки');
-      cy.get(itemConstructor).should('contain', 'Выберите начинку');
+        afterEach(() => {
+            cy.clearCookies();
+        });
     });
-
-    afterEach(() => {
-      cy.clearCookie('accessToken');
-      cy.clearCookie('refreshToken');
-    });
-  });
 });
